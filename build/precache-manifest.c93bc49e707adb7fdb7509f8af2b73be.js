fd638d69137cc387bc39ac5ee0aa4f5bb2464dec
root <root@ip-172-31-35-251.ec2.internal>
2019-12-23 21:55:31 +0000

    a@ aa

---
+++ build/precache-manifest.c93bc49e707adb7fdb7509f8af2b73be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26f3df90c3e28e92b4f3",
+    "url": "/static/js/main.4a407bd8.chunk.js"
+  },
+  {
+    "revision": "f44cfb3208c3ca724ef1",
+    "url": "/static/js/2.9df99257.chunk.js"
+  },
+  {
+    "revision": "26f3df90c3e28e92b4f3",
+    "url": "/static/css/main.bb8aba37.chunk.css"
+  },
+  {
+    "revision": "324aee9e99ac82ebc43ec069cfb81499",
+    "url": "/index.html"
+  }
+];
