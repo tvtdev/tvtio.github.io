80cbd048dd958ecbef3a39867ac6153ab7874298
root <root@ip-172-31-35-251.ec2.internal>
2019-12-23 21:28:41 +0000

    a

---
+++ docs/build/precache-manifest.581ac21365b309f1110c5aa414d1dee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8691ee8ce3be19484ff",
+    "url": "/docs/static/js/runtime~main.f6f3851a.js"
+  },
+  {
+    "revision": "2ece92143150e8437379",
+    "url": "/docs/static/js/main.ff615ab5.chunk.js"
+  },
+  {
+    "revision": "f44cfb3208c3ca724ef1",
+    "url": "/docs/static/js/2.9df99257.chunk.js"
+  },
+  {
+    "revision": "2ece92143150e8437379",
+    "url": "/docs/static/css/main.8fd72500.chunk.css"
+  },
+  {
+    "revision": "23e8a86d4b57cd775ff3be010fbb0eff",
+    "url": "/docs/index.html"
+  }
+];
---
+++ docs/src/Router.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+
+
+function Main() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default Main
