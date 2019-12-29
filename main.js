259aada95b03fdb630fae00b1ca1d21bcd4ec2d9
tvtdev <625747180@qq.com>
2019-12-29 15:28:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const createNewMessageHtml = (username, message) => {
     article.classList.add('message-text')
     article.innerHTML = `<div class ="text"><ul>${username}:${message}</ul></div>`
     var target = document.getElementById('message_list').appendChild(article);
-   // target.parentNode.insertafter(article, target);
+    target.scrollTop = target.scrollHeight;
 }
 
 const onMessage = (message) => {
